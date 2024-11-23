9c4a3e969e5db343469b6fee621f952d0517c0e1
ziyanx02 <ziyanx02@gmail.com>
2024-11-22 22:39:23 -0500

    add dagger

---
+++ rsl_rl/algorithms/dagger.py
@@ -0,0 +1,135 @@
+# SPDX-FileCopyrightText: Copyright (c) 2021 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Copyright (c) 2021 ETH Zurich, Nikita Rudin
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from rsl_rl.modules import ActorCritic, ActorCriticNostd
+from rsl_rl.storage import RolloutStorageDAgger
+
+class DAgger:
+    actor_critic: ActorCriticNostd
+    actor_critic_gt: ActorCritic
+    def __init__(self,
+                 actor_critic,
+                 actor_critic_gt,
+                 num_learning_epochs=1,
+                 num_mini_batches=1,
+                 clip_param=0.2,
+                 gamma=0.998,
+                 lam=0.95,
+                 value_loss_coef=1.0,
+                 entropy_coef=0.0,
+                 learning_rate=1e-3,
+                 max_grad_norm=1.0,
+                 use_clipped_value_loss=True,
+                 schedule="fixed",
+                 desired_kl=0.01,
+                 device='cpu',
+                 ):
+
+        self.device = device
+
+        self.desired_kl = desired_kl
+        self.kl_mean = 0
+        self.schedule = schedule
+        self.learning_rate = learning_rate
+
+        # PPO components
+        self.actor_critic = actor_critic
+        self.actor_critic.to(self.device)
+        self.storage = None # initialized later
+        self.optimizer = optim.Adam(self.actor_critic.parameters(), lr=learning_rate)
+        self.transition = RolloutStorageDAgger.Transition()
+
+        self.actor_critic_gt = actor_critic_gt
+
+        # PPO parameters
+        self.clip_param = clip_param
+        self.num_learning_epochs = num_learning_epochs
+        self.num_mini_batches = num_mini_batches
+        self.value_loss_coef = value_loss_coef
+        self.entropy_coef = entropy_coef
+        self.gamma = gamma
+        self.lam = lam
+        self.max_grad_norm = max_grad_norm
+        self.use_clipped_value_loss = use_clipped_value_loss
+
+        self.actor_critic.train()
+        self.actor_critic_gt.eval()
+
+    def init_storage(self, num_envs, num_transitions_per_env, actor_obs_shape, critic_obs_shape, action_shape):
+        self.storage = RolloutStorageDAgger(num_envs, num_transitions_per_env, actor_obs_shape, critic_obs_shape, action_shape, self.device)
+
+    def test_mode(self):
+        self.actor_critic.test()
+    
+    def train_mode(self):
+        self.actor_critic.train()
+
+    def act(self, obs, critic_obs):
+        # Compute the actions and values
+        self.transition.observations = obs
+        self.transition.actions_gt = self.actor_critic_gt.act(obs).detach()
+        self.transition.actions = self.actor_critic.act(obs[:, :-2]).detach()
+        return self.transition.actions
+    
+    def process_env_step(self):
+        # Record the transition
+        self.storage.add_transitions(self.transition)
+        self.transition.clear()
+    
+    def compute_returns(self, last_critic_obs):
+        last_values= self.actor_critic.evaluate(last_critic_obs).detach()
+        self.storage.compute_returns(last_values, self.gamma, self.lam)
+
+    def update(self):
+        if self.actor_critic.is_recurrent:
+            generator = self.storage.reccurent_mini_batch_generator(self.num_mini_batches, self.num_learning_epochs)
+        else:
+            generator = self.storage.mini_batch_generator(self.num_mini_batches, self.num_learning_epochs)
+        for actions_batch, actions_gt_batch, observations_batch in generator:
+
+                actions = self.actor_critic.actor(observations_batch[..., :-2])
+                dagger_loss = (actions - actions_gt_batch).pow(2).mean()
+
+                loss = dagger_loss
+
+                # Gradient step
+                self.optimizer.zero_grad()
+                loss.backward()
+                nn.utils.clip_grad_norm_(self.actor_critic.parameters(), self.max_grad_norm)
+                self.optimizer.step()
+
+        num_updates = self.num_learning_epochs * self.num_mini_batches
+        self.storage.clear()
+
+        return dagger_loss
