6c643b7dc6e994b8fa63cbb3788070acad9d3de9
ziyanx02 <ziyanx02@gmail.com>
2025-01-24 14:12:18 -0500

    update log

--- rsl_rl/algorithms/ppo.py
+++ rsl_rl/algorithms/ppo.py
@@ -141,7 +141,7 @@ class PPO:
                     self.kl_mean = kl_mean
 
                     if kl_mean > self.desired_kl * 2.0:
-                        self.learning_rate = max(1e-4, self.learning_rate / 1.5)
+                        self.learning_rate = max(1e-5, self.learning_rate / 1.5)
                     elif kl_mean < self.desired_kl / 2.0 and kl_mean > 0.0:
                         self.learning_rate = min(1e-2, self.learning_rate * 1.5)
 
--- rsl_rl/runners/on_policy_runner.py
+++ rsl_rl/runners/on_policy_runner.py
@@ -11,6 +11,7 @@ from collections import deque
 from torch.utils.tensorboard import SummaryWriter as TensorboardSummaryWriter
 import wandb
 import numpy as np
+import math
 
 import rsl_rl
 from rsl_rl.algorithms import PPO
@@ -60,6 +61,7 @@ class OnPolicyRunner:
 
         # Log
         self.log_dir = log_dir
+        self.print_infos = self.cfg.get("print_infos", True)
         self.writer = None
         self.tot_timesteps = 0
         self.tot_time = 0
@@ -98,7 +100,7 @@ class OnPolicyRunner:
         obs, critic_obs = obs.to(self.device), critic_obs.to(self.device)
         self.train_mode()  # switch to train mode (for dropout for example)
 
-        ep_infos = []
+        iter_infos = []
         rewbuffer = deque(maxlen=100)
         lenbuffer = deque(maxlen=100)
         cur_reward_sum = torch.zeros(self.env.num_envs, dtype=torch.float, device=self.device)
@@ -132,10 +134,7 @@ class OnPolicyRunner:
                         # Book keeping
                         # note: we changed logging to use "log" instead of "episode" to avoid confusion with
                         # different types of logging data (rewards, curriculum, etc.)
-                        if "episode" in infos:
-                            ep_infos.append(infos["episode"])
-                        elif "log" in infos:
-                            ep_infos.append(infos["log"])
+                        iter_infos.append(infos.copy())
                         cur_reward_sum += rewards
                         cur_episode_length += 1
                         new_ids = (dones > 0).nonzero(as_tuple=False)
@@ -163,7 +162,7 @@ class OnPolicyRunner:
                     self.start_recording()
             if it % self.save_interval == 0:
                 self.save(os.path.join(self.log_dir, f"model_{it}.pt"))
-            ep_infos.clear()
+            iter_infos.clear()
             if it == start_iter:
                 # obtain all the diff files
                 git_file_paths = store_code_state(self.log_dir, self.git_status_repos)
@@ -179,43 +178,47 @@ class OnPolicyRunner:
         self.tot_time += locs["collection_time"] + locs["learn_time"]
         iteration_time = locs["collection_time"] + locs["learn_time"]
 
+        step = locs["it"]
+        iter_infos = locs["iter_infos"]
         ep_string = ""
-        if locs["ep_infos"]:
-            for key in locs["ep_infos"][0]:
-                infotensor = torch.tensor([], device=self.device)
-                for ep_info in locs["ep_infos"]:
-                    # handle scalar and zero dimensional tensor infos
-                    if key not in ep_info:
-                        continue
-                    if not isinstance(ep_info[key], torch.Tensor):
-                        ep_info[key] = torch.Tensor([ep_info[key]])
-                    if len(ep_info[key].shape) == 0:
-                        ep_info[key] = ep_info[key].unsqueeze(0)
-                    infotensor = torch.cat((infotensor, ep_info[key].to(self.device)))
-                value = torch.mean(infotensor)
-                # log to logger and terminal
-                if "/" in key:
-                    self.writer.add_scalar(key, value, locs["it"])
+        rew_string = ""
+        if len(iter_infos) > 0:
+            if "episode" in iter_infos[0].keys():
+                for key in iter_infos[0]["episode"].keys():
+                    value_list = []
+                    for iter_info in iter_infos:
+                        if iter_info["episode"][key] is not None:
+                            value_list.append(iter_info["episode"][key])
+                    value = torch.mean(torch.tensor(value_list))
+                    value = 0.0 if torch.isnan(value) else value
+                    self.writer.add_scalar(f"Episode/{key}", value, step)
                     ep_string += f"""{f'{key}:':>{pad}} {value:.4f}\n"""
-                else:
-                    self.writer.add_scalar("Episode/" + key, value, locs["it"])
-                    ep_string += f"""{f'Mean episode {key}:':>{pad}} {value:.4f}\n"""
+            if "rewards" in iter_infos[0].keys():
+                for key in iter_infos[0]["rewards"].keys():
+                    value_list = []
+                    for iter_info in iter_infos:
+                        value_list.append(iter_info["rewards"][key])
+                    value = torch.mean(torch.tensor(value_list))
+                    self.writer.add_scalar(f"StepRew/{key}", value, step)
+                    rew_string += f"""{f'{key}:':>{pad}} {value:.4f}\n"""
+
         mean_std = self.alg.actor_critic.std.mean()
         fps = int(self.num_steps_per_env * self.env.num_envs / (locs["collection_time"] + locs["learn_time"]))
 
-        self.writer.add_scalar("Loss/value_function", locs["mean_value_loss"], locs["it"])
-        self.writer.add_scalar("Loss/surrogate", locs["mean_surrogate_loss"], locs["it"])
-        self.writer.add_scalar("Loss/learning_rate", self.alg.learning_rate, locs["it"])
-        self.writer.add_scalar("Loss/kl", self.alg.kl_mean, locs["it"])
-        self.writer.add_scalar("Policy/mean_noise_std", mean_std.item(), locs["it"])
-        self.writer.add_scalar("Perf/total_fps", fps, locs["it"])
-        self.writer.add_scalar("Perf/collection time", locs["collection_time"], locs["it"])
-        self.writer.add_scalar("Perf/learning_time", locs["learn_time"], locs["it"])
+        self.writer.add_scalar("Loss/value_function", locs["mean_value_loss"], step)
+        self.writer.add_scalar("Loss/surrogate", locs["mean_surrogate_loss"], step)
+        self.writer.add_scalar("Loss/learning_rate", self.alg.learning_rate, step)
+        self.writer.add_scalar("Loss/log_learning_rate", math.log(self.alg.learning_rate), step)
+        self.writer.add_scalar("Loss/kl", self.alg.kl_mean, step)
+        self.writer.add_scalar("Loss/mean_noise_std", mean_std.item(), step)
+        self.writer.add_scalar("Perf/total_fps", fps, step)
+        self.writer.add_scalar("Perf/collection time", locs["collection_time"], step)
+        self.writer.add_scalar("Perf/learning_time", locs["learn_time"], step)
         if len(locs["rewbuffer"]) > 0:
-            self.writer.add_scalar("Train/mean_reward", statistics.mean(locs["rewbuffer"]), locs["it"])
-            self.writer.add_scalar("Train/mean_episode_length", statistics.mean(locs["lenbuffer"]), locs["it"])
+            self.writer.add_scalar("Train/mean_episode_reward", statistics.mean(locs["rewbuffer"]), step)
+            self.writer.add_scalar("Train/mean_episode_length", statistics.mean(locs["lenbuffer"]), step)
             if self.logger_type != "wandb":  # wandb does not support non-integer x-axis logging
-                self.writer.add_scalar("Train/mean_reward/time", statistics.mean(locs["rewbuffer"]), self.tot_time)
+                self.writer.add_scalar("Train/mean_episode_reward/time", statistics.mean(locs["rewbuffer"]), self.tot_time)
                 self.writer.add_scalar(
                     "Train/mean_episode_length/time", statistics.mean(locs["lenbuffer"]), self.tot_time
                 )
@@ -249,7 +252,14 @@ class OnPolicyRunner:
             #   f"""{'Mean reward/step:':>{pad}} {locs['mean_reward']:.2f}\n"""
             #   f"""{'Mean episode length/episode:':>{pad}} {locs['mean_trajectory_length']:.2f}\n""")
 
-        log_string += ep_string
+        if self.print_infos:
+            log_string += f"""{'-' * width}\n"""
+            log_string += "Per Episode".center(width, " ") + "\n"
+            log_string += ep_string
+            log_string += f"""{'-' * width}\n"""
+            log_string += "Per Step".center(width, " ") + "\n"
+            log_string += rew_string
+
         log_string += (
             f"""{'-' * width}\n"""
             f"""{'Total timesteps:':>{pad}} {self.tot_timesteps}\n"""
